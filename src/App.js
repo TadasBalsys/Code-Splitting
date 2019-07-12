b7933a97b69d868552dbd042fbc3d7ef07ee84a2
Tadas Balsys <balsys.tadas@gmail.com>
2019-07-12 18:03:57 +0300

    Added Code Splitting in App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React, { Component } from "react";
 import "./App.css";
 import Page1 from "./components/Page1";
-import Page2 from "./components/Page2";
-import Page3 from "./components/Page3";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: "page1"
+      route: "page1",
+      component: ""
     };
   }
 
   onRouteChange = route => {
-    this.setState({ route: route });
+    // No code splitting:
+    // this.setState({ route: route });
+
+    // With code splitting:
+    switch (route) {
+      case "page2":
+        // this import here is only available by webpack, which comes with create-react-app by default.
+        // this import is async. it returns a Promise.
+        import("./components/Page2").then(Page2 => {
+          this.setState({ route: route, component: Page2.default });
+        });
+        break;
+      case "page3":
+        import("./components/Page3").then(Page3 => {
+          this.setState({ route: route, component: Page3.default });
+        });
+        break;
+      default:
+        this.setState({ route: route, component: Page1.default });
+        break;
+    }
   };
 
   render() {
     switch (this.state.route) {
       case "page2":
-        return <Page2 onRouteChange={this.onRouteChange} />;
-
+        return <this.state.component onRouteChange={this.onRouteChange} />;
       case "page3":
-        return <Page3 onRouteChange={this.onRouteChange} />;
-
+        return <this.state.component onRouteChange={this.onRouteChange} />;
       default:
         return <Page1 onRouteChange={this.onRouteChange} />;
     }
